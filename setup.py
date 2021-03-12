1e622cd1e91af9525a66d4d85ed2fe9fd45b3c14
Kris Warner <kdwarn@protonmail.com>
2021-03-12 14:38:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (c) 2014 CoNWeT Lab., Universidad Politécnica de Madrid
+# 2020-2021 DVRPC
 
 # This file is part of OAuth2 CKAN Extension.
 
@@ -18,89 +19,45 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with OAuth2 CKAN Extension.  If not, see <http://www.gnu.org/licenses/>.
 
-import re
-
 from setuptools import setup, find_packages
 
-from ckanext.oauth2 import __version__, __description__
-
-
-PYPI_RST_FILTERS = (
-    # Remove travis ci badge
-    (r'.*travis-ci\.org/.*', ''),
-    # Remove pypip.in badges
-    (r'.*pypip\.in/.*', ''),
-    (r'.*crate\.io/.*', ''),
-    (r'.*coveralls\.io/.*', ''),
-)
-
-
-def rst(filename):
-    '''
-    Load rst file and sanitize it for PyPI.
-    Remove unsupported github tags:
-     - code-block directive
-     - travis ci build badge
-    '''
-    content = open(filename).read()
-    for regex, replacement in PYPI_RST_FILTERS:
-        content = re.sub(regex, replacement, content)
-    return content
-
-
-# long_description = '\n'.join((
-#     rst('README.md'),
-#     rst('CHANGELOG.rst'),
-#     ''
-# ))
+version = "0.8.0"
 
 setup(
-    name='ckanext-oauth2',
-    version=__version__,
-    description=__description__,
-    long_description='''
+    name="ckanext-oauth2",
+    version=version,
+    description="Oauth for CKAN",
+    long_descrition="""
     The OAuth2 extension allows site visitors to login through an OAuth2 server.
-    ''',
-    keywords='CKAN, OAuth2',
-    author='Aitor Magán',
-    author_email='amagan@conwet.com',
-    url='https://github.com/conwetlab/ckanext-oauth2',
-    download_url='https://github.com/conwetlab/ckanext-oauth2/tarball/v' + __version__,
-    license='',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-    namespace_packages=['ckanext'],
+    """,
+    keywords="CKAN, OAuth2",
+    author="Aitor Magán, Kris Warner",
+    author_email="amagan@conwet.com, kdwarn@protonmail.com",
+    url="https://github.com/dvrpc/ckanext-oauth2",
+    license="MIT",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    namespace_packages=["ckanext"],
     include_package_data=True,
     zip_safe=False,
-    setup_requires=[
-        'nose>=1.3.0'
-    ],
+    setup_requires=["nose>=1.3.0"],
     install_requires=[
-        'requests-oauthlib==0.8.0',
-        'pyjwt==1.7.1',
+        "requests-oauthlib==0.8.0",
+        "pyjwt==1.7.1",
     ],
-    tests_require=[
-        'parameterized',
-        'selenium==3.5.0'
-    ],
-    test_suite='nosetests',
-    entry_points={
-        'ckan.plugins': [
-            'oauth2 = ckanext.oauth2.plugin:OAuth2Plugin',
-        ],
-        'nose.plugins': [
-            'pylons = pylons.test:PylonsPlugin'
-        ]
-    },
+    entry_points="""
+        [ckan.plugins]
+        oauth2=ckanext.oauth2.plugin:OAuth2Plugin
+    """,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Session',
-        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Systems Administration :: Authentication/Directory',
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Session",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration :: Authentication/Directory",
     ],
 )
